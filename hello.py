df705c0f8f2723104d7d844b3c6f15e234d9226a
yongB2 <160087434+yongB2@users.noreply.github.com>
2025-04-18 10:23:42 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -2,7 +2,9 @@
 // First: GitHub modification
 // Second: Local repository modification
 
+>>>>>>>> origin/master
 print("hello world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
